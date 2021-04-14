bfda7d61557149d8d04bd3a9c3f791147f8df1fb
Solomon <solomon.asezebhobor@opencii.nl>
2021-04-14 23:23:20 +0200

    Did some scanner things

--- src/Main.java
+++ src/Main.java
@@ -1,22 +1,20 @@
 import Exceptions.ItemDoesNotExistException;
-import store.Order;
-import store.Oven;
-import store.Store;
 
 public class Main {
     public static void main(String[] args) throws ItemDoesNotExistException, InterruptedException {
-        Store store = new Store();
-        Order order = store.createOrder("Kabir");
-        order.addPizza("chicago-pizza", "foo");
-        order.addPizza("veggie-pizza", "te");
-        order.addDecorationToPizza("foo", "sausage");
-        order.addDecorationToPizza("te", "cheddar");
-        System.out.println(order.findPizza("foo").getStatus());
-        System.out.println(order.findPizza("te").getStatus());
-        Oven oven = new Oven();
-        oven.prepareOrder(order);
-        System.out.println(order.findPizza("foo").getStatus());
-        System.out.println(order.findPizza("te").getStatus());
-        System.out.println(oven.isReadyStatus());
+        RunApplication runApplication = new RunApplication();
+//        Store store = new Store();
+//        Order order = store.createOrder("Kabir");
+//        order.addPizza("chicago-pizza", "foo");
+//        order.addPizza("veggie-pizza", "te");
+//        order.addDecorationToPizza("foo", "sausage");
+//        order.addDecorationToPizza("te", "cheddar");
+//        System.out.println(order.findPizza("foo").getStatus());
+//        System.out.println(order.findPizza("te").getStatus());
+//        Oven oven = new Oven();
+//        oven.prepareOrder(order);
+//        System.out.println(order.findPizza("foo").getStatus());
+//        System.out.println(order.findPizza("te").getStatus());
+//        System.out.println(oven.isReadyStatus());
     }
 }
